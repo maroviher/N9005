2e0a199388e09796ea0324d2cc413f21612f58b9
maroviher <qscgy@mail.ru>
2016-01-25 17:59:01 +0100

    build script fix

--- N9005XXUGBOJ1/linux_kernel/build_kernel.sh
+++ N9005XXUGBOJ1/linux_kernel/build_kernel.sh
@@ -8,8 +8,8 @@ make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8974_sec_hlte_eur_defconfig msm8974
 make -C $(pwd) O=output -j5
 
 tools/dtbTool -p output/scripts/dtc/ -s 2048 -o output/arch/arm/boot/ffffff.dtb output/arch/arm/boot/
-tools/mkbootimg --kernel output/arch/arm/boot/zImage --ramdisk boot_image/boot.img-ramdisk.gz --cmdline 'console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x37 ehci-hcd.park=3' --base 0x00000000 --pagesize 2048 --dt output/arch/arm/boot/ffffff.dtb -o boot_image/boot.img
+tools/mkbootimg --tags_offset 0x01e00000 --ramdisk_offset 0x02000000 --kernel output/arch/arm/boot/zImage --ramdisk boot_image/boot.img-ramdisk.gz --cmdline 'console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x37 ehci-hcd.park=3' --base 0x00000000 --pagesize 2048 --dt output/arch/arm/boot/ffffff.dtb -o boot_image/boot.img
 cd boot_image
-tar cf "`date +"%H_%m_%S__%_d-%m-%Y"`.tar" boot.img && rm boot.img
+tar cf "`date +"%H_%M_%S__%_d-%m-%Y"`.tar" boot.img && rm boot.img
 cd -
 #cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
