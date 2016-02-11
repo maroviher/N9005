5ac9c8b05726664cea7344c9bae0298a58f6a7e9
maroviher <qscgy@mail.ru>
2016-02-11 11:37:07 +0100

    build script improvements

--- N9005XXUGBOJ1/linux_kernel/build_kernel.sh
+++ N9005XXUGBOJ1/linux_kernel/build_kernel.sh
@@ -4,11 +4,17 @@ export ARCH=arm
 export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.7/bin/arm-eabi-
 mkdir output
 
-make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8974_sec_hlte_eur_defconfig msm8974_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -C $(pwd) O=output -j5
+BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
+
+make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8974_sec_hlte_eur_defconfig \
+			msm8974_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig || exit -1
+make -C $(pwd) O=output -j$BUILD_JOB_NUMBER || exit -1
 
 tools/dtbTool -p output/scripts/dtc/ -s 2048 -o output/arch/arm/boot/ffffff.dtb output/arch/arm/boot/
-tools/mkbootimg --tags_offset 0x01e00000 --ramdisk_offset 0x02000000 --kernel output/arch/arm/boot/zImage --ramdisk boot_image/boot.img-ramdisk.gz --cmdline 'console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x37 ehci-hcd.park=3' --base 0x00000000 --pagesize 2048 --dt output/arch/arm/boot/ffffff.dtb -o boot_image/boot.img
+tools/mkbootimg --tags_offset 0x01e00000 --ramdisk_offset 0x02000000 --kernel output/arch/arm/boot/zImage \
+			--ramdisk boot_image/boot.img-ramdisk.gz \
+			--cmdline 'console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x37 ehci-hcd.park=3' \
+			--base 0x00000000 --pagesize 2048 --dt output/arch/arm/boot/ffffff.dtb -o boot_image/boot.img
 cd boot_image
 tar cf "`date +"%H_%M_%S__%_d-%m-%Y"`.tar" boot.img && rm boot.img
 cd -
