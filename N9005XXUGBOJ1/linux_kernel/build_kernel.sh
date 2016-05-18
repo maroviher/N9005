d4ab80f0134684c4231dafe283e75a8f84f9e43e
maroviher <qscgy@mail.ru>
2016-05-18 19:34:36 +0200

    added TWRP recovery zip, as output

--- N9005XXUGBOJ1/linux_kernel/build_kernel.sh
+++ N9005XXUGBOJ1/linux_kernel/build_kernel.sh
@@ -15,6 +15,10 @@ tools/mkbootimg --tags_offset 0x01e00000 --ramdisk_offset 0x02000000 --kernel ou
 			--ramdisk boot_image/boot.img-ramdisk.gz \
 			--cmdline 'console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x37 ehci-hcd.park=3' \
 			--base 0x00000000 --pagesize 2048 --dt output/arch/arm/boot/ffffff.dtb -o boot_image/boot.img
-
-tar cf boot_image/"`date +"%H_%M_%S__%_d-%m-%Y"`.tar" -C boot_image/ boot.img && rm boot_image/boot.img
+cd boot_image/
+DATE_STR=`date +"%H_%M_%S__%_d-%m-%Y"`
+cp zip_recovery.zip $DATE_STR.zip
+zip -u $DATE_STR.zip boot.img
+cd -
+tar cf boot_image/$DATE_STR.tar -C boot_image/ boot.img && rm boot_image/boot.img
 #cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
